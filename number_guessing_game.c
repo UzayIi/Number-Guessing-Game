0fbe1f55bc23017249f36bd35f115e6f48ba7860
Umut Arslan <arslanumut03@gmail.com>
2025-07-31 12:47:45 +0300

    Max triess added

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
 
     srand(time(NULL));
 
@@ -11,33 +12,43 @@ int main() {
     int min = 1;
     int max = 100;
     int answer = (rand() % (max - min + 1)) + min;
+    int max_tries = 7;
 
     printf("*** NUMBER GUESSING GAME ***\n\n");
+    printf("You have 7 tries to find it. Good Luck!\n\n");
 
-    do{
+    do
+    {
         printf("Guess a number between %d - %d: ", min, max);
         scanf("%d", &guess);
         tries++;
 
-        if (guess < answer){
+        if (guess < answer)
+        {
             printf("TOO LOW!\n");
         }
-        else if(guess > answer){
+        else if (guess > answer)
+        {
             printf("TOO HIGH!\n");
         }
-        else{
+        else
+        {
             printf("CORRECT!\n\n");
         }
-        
 
-    } while (guess !=answer);
+    } while (tries < max_tries);
 
-    printf("The answer is %d.\n\n", answer);
-    printf("It took you %d tries to find it.", tries);
+    if (guess == answer)
+    {
+        printf("The answer is %d.\n\n", answer);
+        printf("It took you %d tries to find it.\n", tries);
+    }
+    else{
+        printf("\nOut of tries! Correct answer was %d.\n", answer);
+    }
     
 
-
-
+    
 
     return 0;
 }
